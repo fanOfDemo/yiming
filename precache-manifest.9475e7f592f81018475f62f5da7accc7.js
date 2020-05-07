6a4cc17037f53b7e5208312bcbfbbd105ca299f0
wengyiming <wengyiming@meituan.com>
2020-05-07 20:59:33 +0800

    Updates

---
+++ precache-manifest.9475e7f592f81018475f62f5da7accc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be5fcf7c0fe7160a0e6d2799bbdb4f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1637114b4e97de5b9348",
+    "url": "./static/css/2.5674ce1a.chunk.css"
+  },
+  {
+    "revision": "c0dc42ac0ff4c096113c",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1637114b4e97de5b9348",
+    "url": "./static/js/2.bfc53c8a.chunk.js"
+  },
+  {
+    "revision": "8b869f981bd45dc95df492c2e1e32675",
+    "url": "./static/js/2.bfc53c8a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c0dc42ac0ff4c096113c",
+    "url": "./static/js/main.eecab18d.chunk.js"
+  },
+  {
+    "revision": "d43c4bfa027c658a40d9",
+    "url": "./static/js/runtime-main.f194559f.js"
+  }
+]);
